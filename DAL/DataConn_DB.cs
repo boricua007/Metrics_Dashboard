5a80fe3400cf930d4c2b9b43de40b0091e5e749a
Daisy <daisyallen89@comcast.net>
2017-12-14 13:32:11 -0500

    - Enabled auto-migration in Configuration.cs file - Added LazyLoading in DataConn_DB file - Updated database to create first table (dbo.Clients)

--- DAL/DataConn_DB.cs
+++ DAL/DataConn_DB.cs
@@ -7,5 +7,16 @@ namespace DAL
     public class DataConn_DB : DbContext
     {
         public DbSet<Client> clients { get; set; }
+
+
+
+
+
+
+        public DataConn_DB( ) : base( "Metrics_Dashboard" )
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
     }
 }
