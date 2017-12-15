b231a9c65d91280d7ff02742ce0b7c7a928ffa35
Daisy <daisyallen89@comcast.net>
2017-12-15 12:19:48 -0500

    - Angular html views created (client, login, nav, register, success) - Angular controllers created (client.js, registration.js, success.js)

---
+++ WebApplication/Scripts/app.js
@@ -0,0 +1,24 @@
+﻿var myApp = angular.module('dashboardApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'RegistrationController'
+        }).
+        when('/register', {
+            templateUrl: 'views/register.html',
+            controller: 'RegistrationController'
+        }).
+        when('/client', {
+            templateUrl: 'views/client.html',
+            controller: 'ClientController'
+        }).
+        when('/success', {
+            templateUrl: 'views/success.html',
+            controller: 'SuccessController'
+        }).
+        otherwise({
+            redirectTo: '/login'
+        });
+}]);
